d18825efa957bdfa423d63367eee343dac960289
Joel Bakken <jkbetc@gmail.com>
2017-06-09 15:48:19 -0700

    added more to the readme file and made the page a little more colorful with background colors and a picture on the top.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
- var endNumber = parseInt($("#endNumber").val());
-
-function pingpong(){
-  for(var startNumber = 1;startNumber<=endNumber;startNumber++){
-    if(startNumber%5 === 0 && startNumber%3 === 0){
-        print('pingpong');
-    } else if(startNumber%3 === 0){
-        print('ping');
-    } else if(startNumber%5 === 0){
-        print('pong');
-    } else {
-        print(startNumber);
-    }
-  }
-}
-
-var r = document.getElementById('result');
-
-function print(s){
-  r.innerHTML += s + '<br>';
-}
-
-pingpong();
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+      var endNumber = parseInt($("#endNumber").val());
+        function pingpong() {
+          for (var startNumber = 1; startNumber <= endNumber; startNumber++) {
+            if (startNumber % 5 === 0 && startNumber % 3 === 0) {
+              print('pingpong');
+            } else if (startNumber % 3 === 0) {
+              print('ping');
+            } else if (startNumber % 5 === 0) {
+              print('pong');
+            } else {
+              print(resuts);
+            }
+          }
+        }
+      var results = document.getElementById('results');
+      function print(r) {
+        r.innerHTML += s + '<br>';
+      }
+    });
+});
