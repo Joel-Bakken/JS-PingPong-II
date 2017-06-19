c27b52acede447c4d407d43cc035c91efe31f2b7
Joel Bakken <jkbetc@gmail.com>
2017-06-18 17:44:21 -0700

    add additional code that did not work, but proves I am trying

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,22 @@ $(document).ready(function() {
     var results = [startNumber, endNumber];
     var endNumber = parseInt($("#endNumber").val());
 
-          for (var startNumber = 1; startNumber <= endNumber; startNumber++) {
-            alert(startNumber)
-            if (startNumber % 5 === 0 && startNumber % 3 === 0) {
-              $("#results").push('pingpong');
-            } else if (startNumber % 3 === 0) {
-              $("#results").push('ping');
-            } else if (startNumber % 5 === 0) {
-              $("#results").push('pong');
-            } else {
-              $("#results").push();
-              alert("#results");
-            }
-          }
-          $("#btn").click(pingpong).text();
+      for (var startNumber = 1; startNumber <= endNumber; startNumber++) {
+        if (startNumber % 5 === 0 && startNumber % 3 === 0) {
+          $("#results").push('pingpong');
+        } else if (startNumber % 3 === 0) {
+          $("#results").push('ping');
+        } else if (startNumber % 5 === 0) {
+          $("#results").push('pong');
+        } else {
+          $("#results").push(results);
         }
+      }
+      $("#btn").click(pingpong); {
+         $("#result").text(result);
+      }
+
+    }
 
   });
 });
