00b991cce5e8398d47945dc11e051372926fba76
Joe Kramer and Joel Bakken <joe+joel@>
2017-08-01 09:47:46 -0700

    lesson follow along

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
+var utilities = require('gulp-util');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var buildProduction = utilities.env.production;
+var del = require('del');
+
 
 //more dependencies will be added here.
 gulp.task('concatInterface', function() {
@@ -15,4 +20,22 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
