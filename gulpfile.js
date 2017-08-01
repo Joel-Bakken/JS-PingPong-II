ba6b8e9d4bbade9de1f8ff146e716308eb558de2
Joel Bakken and Joe Kramer <joel@>
2017-08-01 13:03:19 -0700

    lesson follow along

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,17 @@ var uglify = require('gulp-uglify');
 var buildProduction = utilities.env.production;
 var del = require('del');
 var jshint = require('gulp-jshint');
-
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 //more dependencies will be added here.
 gulp.task('concatInterface', function() {
@@ -39,7 +49,23 @@ gulp.task("build", ['clean'], function(){
   } else {
     gulp.start('jsBrowserify');
   }
+  gulp.start('bower');
 });
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
